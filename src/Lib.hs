d0d63cdb8d7e1cf80f80229a8ae6d079969588b4
Joe Hillenbrand <joehillen@gmail.com>
2016-12-09 12:33:07 -0800

    add stdout logging

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,15 +8,20 @@ module Lib
 import           Control.Applicative
 import           Control.Concurrent
 import           Control.Monad
-import qualified Data.ByteString           as S
-import           Data.ByteString.Lazy      hiding (filter, length, map,
-                                            putStrLn)
+import           Control.Monad.IO.Class     (liftIO)
+import qualified Data.ByteString            as S
+import qualified Data.ByteString.Lazy       as SL
 import           Data.IP
 import           Data.Maybe
 import           Data.Monoid
+import qualified Data.Text                  as T
+import           Data.Text.Encoding         (decodeUtf8)
+import           Log
+import           Log.Backend.ElasticSearch
+import           Log.Backend.StandardOutput
 import           Network.BSD
 import           Network.DNS
-import           Network.Socket            hiding (recvFrom)
+import           Network.Socket             hiding (recvFrom)
 import           Network.Socket.ByteString
 import           System.Environment
 import           System.Timeout
@@ -33,49 +38,55 @@ data Conf = Conf
   }
   deriving Show
 
-timeout' :: String -> Int -> IO a -> IO (Maybe a)
-timeout' msg tm io = do
-  result <- timeout tm io
-  maybe (putStrLn msg) (const $ return ()) result
+timeout' :: SockAddr -> Int -> IO a -> LogT IO (Maybe a)
+timeout' addr tm io = do
+  result <- liftIO $ timeout tm io
+  when (isNothing result) $
+    logAttention_ $ "timeout sending to "<>(T.pack $ show addr)
   return result
 
 
 handleRequest :: Conf -> DNSMessage -> Maybe DNSMessage
-handleRequest conf req = parseHosts
+handleRequest conf req = parseHosts <$> mq
  where
   filterA = filter ((==A) . qtype)
   ident = identifier . header $ req
   mq = listToMaybe . filterA . question $ req
-  parseHosts =
-    case mq of
-      Nothing -> Nothing
-      Just q ->
-        let an = maybeToList $ parseDomain (confDomain conf) $ qname q
-            rsp = responseA ident q an
-            setTTL rr = rr { rrttl = confTTL conf }
-          in Just $
-             rsp
-             { header =
-                let hd   = header rsp
-                    flgs = (flags hd)
-                           { recAvailable = False
-                           , authAnswer = not $ Prelude.null an
-                           }
-                 in hd { flags = flgs }
-             , answer = map setTTL $ answer rsp
-             }
+  parseHosts q =
+    let ip = maybeToList $ parseDomain (confDomain conf) $ qname q
+        rsp = responseA ident q ip
+        setTTL rr = rr { rrttl = confTTL conf }
+        hd = header rsp
+        flgs = (flags hd)
+               { recAvailable = False
+               , authAnswer = not $ Prelude.null ip
+               }
+     in rsp
+        { header = hd { flags = flgs }
+        , answer = map setTTL $ answer rsp
+        }
 
 
-handlePacket :: Conf -> Socket -> SockAddr -> S.ByteString -> IO ()
+handlePacket :: Conf -> Socket -> SockAddr -> S.ByteString -> LogT IO ()
 handlePacket conf@Conf{..} sock addr bs =
-  case decode $ fromChunks [bs] of
+  case decode $ SL.fromChunks [bs] of
     Right req -> do
       case handleRequest conf req of
-        Just rsp ->
-          let packet = mconcat . toChunks $ encode rsp
-          in void $ timeout' "send timeout" confTimeout (sendAllTo sock packet addr)
+        Just rsp -> do
+          let packet = mconcat . SL.toChunks $ encode rsp
+          void $ timeout' addr confTimeout (sendAllTo sock packet addr)
+          case answer rsp of
+            [] -> return ()
+            (ResourceRecord { rdata = (RD_A ip) }):_ ->
+              logInfo "" $ object [
+                  "from" .= (show addr)
+                , "question"  .= (decodeUtf8 . qname . head . question $ req)
+                , "answer" .= show ip
+                ]
+            _ -> return ()
         Nothing -> return ()
-    Left msg -> putStrLn msg
+    Left msg ->
+      logAttention "Failed to decode message" $ object [ "message" .= msg ]
 
 
 serveDNS :: Domain -> Int -> IO ()
@@ -96,6 +107,7 @@ serveDNS domain port = withSocketsDo $ do
   addrinfo <- maybe (fail "no addr info") return (listToMaybe addrinfos)
   sock <- socket (addrFamily addrinfo) Datagram defaultProtocol
   bind sock (addrAddress addrinfo)
-  forever $ do
-    (bs, addr) <- recvFrom sock (confBufSize conf)
-    forkIO $ handlePacket conf sock addr bs
+  withSimpleStdOutLogger $ \logger ->
+    forever $ do
+      (bs, addr) <- recvFrom sock (confBufSize conf)
+      forkIO $ runLogT "" logger $ handlePacket conf sock addr bs
