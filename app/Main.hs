dd0d4a39f375f3c6aa643ee11b81afb9c53bc4b4
Oleksandr Kylymnychenko <oleksandr.kylymnychenko@elastic.co>
2019-11-19 10:23:01 +0100

    [hipio] remove Elasticsearch dependencies
    This commit cleans up all the ES deps and remove the code of
    elasticsearch logging. From now on we will rely only on stdout logging.

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ module Main where
 import qualified Data.ByteString.Char8    as B8
 import           Data.Text                (Text)
 import qualified Data.Text                as T
-import           Database.V5.Bloodhound   (EsPassword (..), EsUsername (..))
 import           Lib
 import           Options.Applicative
 import           Options.Applicative.Text (textOption)
@@ -13,18 +12,6 @@ import           System.Environment
 main :: IO ()
 main = do
   opts <- execParser opts
-  esconf <-
-    case optsEsUrl opts of
-      Nothing -> return Nothing
-      Just url -> do
-        mbUN <- lookupEnv "ES_USER"
-        login <-
-          case mbUN of
-            Nothing -> return Nothing
-            Just un -> do
-              !pw <- getEnv "ES_PASS"
-              return $ Just (EsUsername $ T.pack un, EsPassword $ T.pack pw)
-        return $ Just (url, login)
   let ns = case optsNSs opts of
              [] -> [optsDomain opts]
              ns -> ns
@@ -34,7 +21,6 @@ main = do
     (optsAs opts)
     ns
     (optsEmail opts)
-    esconf
  where
    opts = info (helper <*> options)
      ( fullDesc
@@ -48,7 +34,6 @@ main = do
 data Options = Options
   { optsDomain :: String
   , optsPort   :: Int
-  , optsEsUrl  :: Maybe Text
   , optsAs     :: [String]
   , optsNSs    :: [String]
   , optsEmail  :: String
@@ -70,13 +55,6 @@ options =
     <> showDefault
     <> help "Listening port."
     )
-  <*>
-    optional (
-      textOption
-      (  long "es"
-      <> help "Elasticsearch URL for Logging. Set `ES_USER` and `ES_PASS` environment variables for Basic Auth."
-      <> metavar "URL"
-      ))
   <*>
     many (
       strOption
